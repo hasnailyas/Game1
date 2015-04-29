6ea925f0124f22ba171ff69bc7e47500a293a998
hasnailyas <alyahvanihasna@gmail.com>
2015-04-29 16:35:29 +0800

    laporan 1 game versi 1

---
+++ Assets/jump.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jump : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().AddForce (Vector3.up*150);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().AddForce (Vector3.right*100);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			GetComponent<Rigidbody> ().AddForce (Vector3.left*100);
+		}
+	}
+}
